6262966305c2df092b8e15caa249fc8485443a63
Mike Holm and George Miller <michael.holm@ca.com>
2016-12-08 10:28:48 -0700

    first commit! yay!

---
+++ README.md
@@ -0,0 +1,15 @@
+# Async-Exec
+
+Have you ever wanted to exec things asyncly? Now you can!
+
+## Usage
+
+`npm install -S async-exec`
+
+Then in your code
+
+    import exec from 'async-exec';
+    
+    await exec(`osascript -e "set volume ${scaledVolume}"`);
+    const tsFiles = await execCommand(`find . -name "*.ts"`);
+    
---
+++ index.ts
@@ -0,0 +1,23 @@
+import { exec } from 'child_process';
+
+function printLines(data: Object) {
+  data.toString().split('\n').forEach(line => {
+    console.log(line);
+  });
+}
+
+export default function(command: string, logOutput: boolean = false): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const process = exec(command, (error: any, stdout: any, stderr: any) => {
+      if (error) {
+        return reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+
+    if (logOutput) {
+      process.on('data', (data: Object) => printLines(data));
+    }
+  });
+}
